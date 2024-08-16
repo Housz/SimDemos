5611c076a269135a2588276ec4cc051568493811
Housz <Housz@Housz.cn>
2024-08-16 09:01:46 +0800

    update dashboard

--- 2.DashBoard/main.js
+++ 2.DashBoard/main.js
@@ -1,45 +1,20 @@
-var mycanvas = document.getElementById("mycanvas");
-var ctx = mycanvas.getContext("2d");
-
-const WIDTH  = 800;
-const HEIGHT = 800;
-
-const TIME_STEP = 0.1;
-const G = 10;
-
-var Ball = 
-{
-    x: 0 + 800 / 2,
-    y: 0,
-    vx: 0,
-    vy: 0,
-    mass: 10,
-    radius: 25,
-    color: 'blue',
-    draw: function() 
-    {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-}
+let mycanvas = document.getElementById("mycanvas");
+let ctx = mycanvas.getContext("2d");
 
 
-var init_main = function ()
+let init_main = function ()
 {
     mycanvas.width = WIDTH;
     mycanvas.height = HEIGHT;
 }
 
-var draw_main = function ()
+let draw_main = function ()
 {
     window.requestAnimationFrame(simulation);
 }
 
 
-var simulation = function () 
+let simulation = function () 
 {
     // clear canvas
     ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
